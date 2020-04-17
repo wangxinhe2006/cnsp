b568e69eb3b25f7dda7574bd1dd65c89b9b7e355
汪心禾 <wangxinhe06@gmail.com>
2020-04-17 17:28:28 +0800

    Support google.cn

---
+++ cnsp/__init__.py
@@ -0,0 +1,12 @@
+import json
+from urllib.request import urlopen
+from urllib.parse import urlencode
+
+
+def google(q):
+    'https://www.google.cn/complete/search'
+    f = urlopen(
+        'https://www.google.cn/complete/search'
+        f"?{urlencode({'q': q, 'client': 'psy-ab'})}"
+    )
+    return [p[0] for p in json.loads(f.read().decode(f.headers.get_content_charset()))[1]]
